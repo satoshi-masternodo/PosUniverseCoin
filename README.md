c839156d15dcae765356db038d9d5266d490f919
satoshi-masternodo <admin@dvizh.net>
2018-07-02 20:20:24 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+
+# Posuniversecoin development tree
+
+Posuniversecoin is a PoS-based cryptocurrency.
+
+Rpcport - 45522
+
+PSU is dependent upon libsecp256k1 by sipa, the sources for which can be found here:
+https://github.com/bitcoin/secp256k1
+
+Install
+-------
+sudo apt-get update
+
+sudo apt-get upgrade 
+
+sudo apt-get install nano ntp unzip git build-essential libssl-dev libdb-dev libdb++-dev libboost-all-dev libqrencode-dev 
+aptitude install miniupnpc libminiupnpc-dev
+
+sudo apt-get install libgmp3-dev
+
+cd posuniversecoin
+cd src
+cd leveldb
+chmod 755 build_detect_platform
+make clean
+make libleveldb.a libmemenv.a
+
+cd ..
+
+make -f makefile.unix
+
+strip posuniversecoind
+
+cp posuniversecoind /usr/local/bin
+
+posuniversecoind -daemon
+-----------------
+rpcuser=rpc_posuniversecoinu
+rpcpassword=password
+rpcallowip=127.0.0.1
+listen=1
+server=1
+txindex=1
+daemon=1
+
+posuniversecoind stop
+--------------
+posuniversecoind -daemon
+-----------------
---
+++ src/chainparams.cpp
@@ -0,0 +1,198 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "assert.h"
+
+#include "chainparams.h"
+#include "main.h"
+#include "util.h"
+
+#include <boost/assign/list_of.hpp>
+
+using namespace boost::assign;
+
+struct SeedSpec6 {
+	uint8_t addr[16];
+	uint16_t port;
+};
+
+#include "chainparamsseeds.h"
+//
+// Main network
+//
+
+// Convert the pnSeeds array into usable address objects.
+static void convertSeeds(std::vector<CAddress> &vSeedsOut, const unsigned int *data, unsigned int count, int port)
+{
+	// It'll only connect to one or two seed nodes because once it connects,
+	// it'll get a pile of addresses with newer timestamps.
+	// Seed nodes are given a random 'last seen time' of between one and two
+	// weeks ago.
+	const int64_t nOneWeek = 7 * 24 * 60 * 60;
+	for (unsigned int k = 0; k < count; ++k)
+	{
+		struct in_addr ip;
+		unsigned int i = data[k], t;
+
+		// -- convert to big endian
+		t = (i & 0x000000ff) << 24u
+			| (i & 0x0000ff00) << 8u
+			| (i & 0x00ff0000) >> 8u
+			| (i & 0xff000000) >> 24u;
+
+		memcpy(&ip, &t, sizeof(ip));
+
+		CAddress addr(CService(ip, port));
+		addr.nTime = GetTime() - GetRand(nOneWeek) - nOneWeek;
+		vSeedsOut.push_back(addr);
+	}
+}
+
+class CMainParams : public CChainParams {
+public:
+	CMainParams() {
+		// The message start string is designed to be unlikely to occur in normal data.
+		// The characters are rarely used upper ASCII, not valid as UTF-8, and produce
+		// a large 4-byte int at any alignment.
+		pchMessageStart[0] = 0xb9;
+		pchMessageStart[1] = 0xba;
+		pchMessageStart[2] = 0xfb;
+		pchMessageStart[3] = 0x63;
+		vAlertPubKey = ParseHex("0471882ef15b10f4e42d950dc074741a9abcc982051f9a5af090d6a6dee5bdc43880c31723fd2529de69a46a82b9925abdb63c9a9053b4c2097b1de9ae0e44576a");
+		nDefaultPort = 45521;
+		nRPCPort = 45522;
+		bnProofOfWorkLimit = CBigNum(~uint256(0) >> 16);
+		
+		const char* pszTimestamp = "Pos 2 universe coin France Warns of Several Unauthorized Cryptocurrency Platforms";
+		std::vector<CTxIn> vin;
+		vin.resize(1);
+		vin[0].scriptSig = CScript() << 0 << CBigNum(42) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+		std::vector<CTxOut> vout;
+		vout.resize(1);
+		vout[0].SetEmpty();
+		CTransaction txNew(1, 1530122044, vin, vout, 0);
+		genesis.vtx.push_back(txNew);
+		genesis.hashPrevBlock = 0;
+		genesis.hashMerkleRoot = genesis.BuildMerkleTree();
+		genesis.nVersion = 1;
+		genesis.nTime = 1530122044;
+		genesis.nBits = 520159231;
+		genesis.nNonce = 419548;
+
+		hashGenesisBlock = genesis.GetHash();
+
+		assert(genesis.hashMerkleRoot == uint256("0xec31118e8832957f6a129fabee537f2294002614e1c27b3fc8479224254f2d80"));
+		assert(hashGenesisBlock == uint256("0x0000a53760664a680b5fad6fe1d12c8c0c7ea204eb68d625a6bd4287a7c3c32f"));
+
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 55);
+		base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 85);
+		base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 153);
+		base58Prefixes[STEALTH_ADDRESS] = std::vector<unsigned char>(1, 40);
+		base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
+		base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
+
+        vSeeds.push_back(CDNSSeedData("0",  "185.180.198.52"));
+        vSeeds.push_back(CDNSSeedData("1",  "185.223.95.125"));
+		vSeeds.push_back(CDNSSeedData("178.57.218.104",  "178.57.218.104"));
+		convertSeeds(vFixedSeeds, pnSeed, ARRAYLEN(pnSeed), nDefaultPort);
+
+		nPoolMaxTransactions = 3;
+		//strSporkKey = "046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e";
+		//strMasternodePaymentsPubKey = "046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e";
+		strDarksendPoolDummyAddress = "PNi7j3jzcEAE4jKyRHyvH1fSjZgMQitAjQ";
+		nLastPOWBlock = 50001;
+		nPOSStartBlock = 9000;
+	}
+
+
+	virtual const CBlock& GenesisBlock() const { return genesis; }
+	virtual Network NetworkID() const { return CChainParams::MAIN; }
+
+	virtual const vector<CAddress>& FixedSeeds() const {
+		return vFixedSeeds;
+	}
+protected:
+	CBlock genesis;
+	vector<CAddress> vFixedSeeds;
+};
+static CMainParams mainParams;
+
+
+//
+// Testnet
+//
+
+class CTestNetParams : public CMainParams {
+public:
+	CTestNetParams() {
+		// The message start string is designed to be unlikely to occur in normal data.
+		// The characters are rarely used upper ASCII, not valid as UTF-8, and produce
+		// a large 4-byte int at any alignment.
+		pchMessageStart[0] = 0x1d;
+		pchMessageStart[1] = 0x7e;
+		pchMessageStart[2] = 0xa6;
+		pchMessageStart[3] = 0x2c;
+		bnProofOfWorkLimit = CBigNum(~uint256(0) >> 16);
+		vAlertPubKey = ParseHex("0471882ef15b10f4e42d950dc074741a9abcc982051f9a5af090d6a6dee5bdc43880c31723fd2529de69a46a82b9925abdb63c9a9053b4c2097b1de9ae0e44576a");
+		nDefaultPort = 20115;
+		nRPCPort = 21226;
+		strDataDir = "testnet";
+		// Modify the testnet genesis block so the timestamp is valid for a later start.
+		genesis.nBits = 520159231;
+		genesis.nNonce = 419548;    
+
+		assert(hashGenesisBlock == uint256("0x0000a53760664a680b5fad6fe1d12c8c0c7ea204eb68d625a6bd4287a7c3c32f"));
+
+		vFixedSeeds.clear();
+		vSeeds.clear();
+
+		base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 127);
+		base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 196);
+		base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 239);
+		base58Prefixes[STEALTH_ADDRESS] = std::vector<unsigned char>(1, 40);
+		base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x35)(0x87)(0xCF).convert_to_container<std::vector<unsigned char> >();
+		base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x35)(0x83)(0x94).convert_to_container<std::vector<unsigned char> >();
+
+		convertSeeds(vFixedSeeds, pnTestnetSeed, ARRAYLEN(pnTestnetSeed), nDefaultPort);
+
+		nLastPOWBlock = 0x7fffffff;
+	}
+	virtual Network NetworkID() const { return CChainParams::TESTNET; }
+};
+static CTestNetParams testNetParams;
+
+
+static CChainParams *pCurrentParams = &mainParams;
+
+const CChainParams &Params() {
+	return *pCurrentParams;
+}
+
+void SelectParams(CChainParams::Network network) {
+	switch (network) {
+	case CChainParams::MAIN:
+		pCurrentParams = &mainParams;
+		break;
+	case CChainParams::TESTNET:
+		pCurrentParams = &testNetParams;
+		break;
+	default:
+		assert(false && "Unimplemented network");
+		return;
+	}
+}
+
+bool SelectParamsFromCommandLine() {
+
+	bool fTestNet = GetBoolArg("-testnet", false);
+
+	if (fTestNet) {
+		SelectParams(CChainParams::TESTNET);
+	}
+	else {
+		SelectParams(CChainParams::MAIN);
+	}
+	return true;
+}
